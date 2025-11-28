830fa753505dae024607896ea038be34fd766e15
Self-Developer <git.suraj.96@gmail.com>
2025-11-28 17:36:35 +0530

    Adding Videocall system

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -181,7 +181,7 @@ let remoteStream;
 let answerUrl;
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
     remoteStream = new MediaStream()
     document.getElementById('user-1').srcObject = localStream
     document.getElementById('user-2').srcObject = remoteStream
